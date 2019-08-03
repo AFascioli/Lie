503043e6ef4487b707be94e21cf6bee866406620
Mmanzi <mayramanzi95@gmail.com>
2019-08-03 16:16:53 -0300

    Cambios en interfaz

--- proyectoicr/src/app/estudiantes/calificaciones-estudiantes/calificaciones-estudiantes.component.ts
+++ proyectoicr/src/app/estudiantes/calificaciones-estudiantes/calificaciones-estudiantes.component.ts
@@ -1,4 +1,7 @@
+import { EstudiantesService } from 'src/app/estudiantes/estudiante.service';
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef, MatDialog, MatDialogConfig, MatSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-calificaciones-estudiantes',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calificaciones-estudiantes.component.css']
 })
 export class CalificacionesEstudiantesComponent implements OnInit {
+  
 
   constructor() { }
 
   ngOnInit() {
+
+
+  }
+
+onCancelar() {
+
   }
 
 }
