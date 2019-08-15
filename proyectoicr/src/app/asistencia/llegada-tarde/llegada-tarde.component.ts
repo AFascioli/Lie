0c9cdd2c9959093dd168d9152506aa237b8cd8ba
Mmanzi <mayramanzi95@gmail.com>
2019-08-15 20:02:28 -0300

    interfaz llegada tarde

--- proyectoicr/src/app/asistencia/llegada-tarde/llegada-tarde.component.ts
+++ proyectoicr/src/app/asistencia/llegada-tarde/llegada-tarde.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { EstudiantesService } from "src/app/estudiantes/estudiante.service";
-import { MatDialogRef, MatDialog } from "@angular/material";
+import { MatDialogRef, MatDialog, MAT_DIALOG_DATA, MatDialogConfig } from "@angular/material";
 import { Router } from "@angular/router";
 
 
@@ -14,17 +14,61 @@ export class LlegadaTardeComponent implements OnInit {
   apellidoEstudiante: string;
   nombreEstudiante: string;
   _idEstudiante: string;
-  antes10am: Boolean = true;
-  ///matConfig = new MatDialogConfig();
+  matConfig = new MatDialogConfig();
   constructor(private servicio: EstudiantesService, public popup: MatDialog) { }
 
   ngOnInit() {
     this.fechaActual = new Date();
-    //this.apellidoEstudiante= this.servicio.estudianteSeleccionado.apellido;
-    //this.nombreEstudiante= this.servicio.estudianteSeleccionado.nombre;
-    //this._idEstudiante= this.servicio.estudianteSeleccionado._id;
-    //this.validarHora();
+    this.apellidoEstudiante= this.servicio.estudianteSeleccionado.apellido;
+    this.nombreEstudiante= this.servicio.estudianteSeleccionado.nombre;
+    this._idEstudiante= this.servicio.estudianteSeleccionado._id;
+  }
+}
+
+@Component({
+  selector: "app-llegadaTarde-popup",
+  templateUrl: "./llegadaTarde-popup.component.html",
+  styleUrls: ['./llegada-tarde.component.css']
+})
+export class LlegadaTardePopupComponent {
+  tipoPopup: string;
+  IdEstudiante: string;
+  exito: boolean = false;
+
+  constructor(
+    public dialogRef: MatDialogRef<LlegadaTardePopupComponent>,
+    public router: Router,
+    public servicio: EstudiantesService,
+    @Inject(MAT_DIALOG_DATA) data
+  ) {
+    this.tipoPopup = data.tipoPopup;
+    this.IdEstudiante= data.IdEstudiante;
 
   }
 
+  //Vuelve al menu principal
+  onYesCancelarClick(): void {
+    this.router.navigate(["menuLateral/home"]);
+    this.dialogRef.close();
+  }
+
+  //Cierra el popup y vuelve a la interfaz de retiro
+  onNoCancelarConfirmarClick(): void {
+    this.dialogRef.close();
+  }
+
+  //Confirma el llegada tarde  para el estudiante
+  //onYesConfirmarClick(): void {
+   
+   // });
+//  }
+
+  //Si fue exitosa la operacion vuelve al menu principal, sino vuelve a la interfaz de retiro
+  onOkConfirmarClick() {
+    if (this.exito) {
+      this.router.navigate(["menuLateral/home"]);
+      this.dialogRef.close();
+    }
+    this.dialogRef.close();
+  }
 }
