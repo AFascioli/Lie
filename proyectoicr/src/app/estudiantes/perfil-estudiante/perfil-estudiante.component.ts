df21cfb6f283b595a6cd01d7879d69d775e3ae4b
Mmanzi <mayramanzi95@gmail.com>
2019-09-12 19:38:01 -0300

    perfil estudiante sanciones

--- proyectoicr/src/app/estudiantes/perfil-estudiante/perfil-estudiante.component.ts
+++ proyectoicr/src/app/estudiantes/perfil-estudiante/perfil-estudiante.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EstudiantesService } from "../estudiante.service";
 
+
+
 @Component({
   selector: 'app-perfil-estudiante',
   templateUrl: './perfil-estudiante.component.html',
@@ -10,7 +12,14 @@ export class PerfilEstudianteComponent implements OnInit {
   apellidoEstudiante: string;
   nombreEstudiante: string;
   _idEstudiante: string;
+  displayedColumns: string[] = ["tipo", "cantidad",];
+
+
+
+
   constructor(public servicio: EstudiantesService) { }
+
+
 
   ngOnInit() {
     this.apellidoEstudiante = this.servicio.estudianteSeleccionado.apellido;
@@ -19,3 +28,4 @@ export class PerfilEstudianteComponent implements OnInit {
   }
 
 }
+
