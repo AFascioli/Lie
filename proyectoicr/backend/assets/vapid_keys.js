567bd2c3c7a9f4d7c05c5b79ab4e41584e1c5cbe
AFascioli <agufascioli@gmail.com>
2019-09-23 13:23:48 -0300

    Arreglos en organización y keys-vapid

--- proyectoicr/backend/assets/vapid_keys.js
+++ proyectoicr/backend/assets/vapid_keys.js
@@ -1,3 +1,4 @@
-export const vapid_public_key =
+module.exports.vapid_public_key =
   "BMlC2dLJTBP6T1GCl3S3sDBmhERNVcjN7ff2a6JAoOg8bA_qXjikveleRwjz0Zn8c9-58mnrNo2K4p07UPK0DKQ";
-export const vapid_private_key = "nvrxnM7juFwnVOaGA0gY6S7KWKi69ZVC0z7jU5bmQss";
+module.exports.vapid_private_key =
+  "nvrxnM7juFwnVOaGA0gY6S7KWKi69ZVC0z7jU5bmQss";
--- proyectoicr/backend/classes/suscripcion.js
+++ proyectoicr/backend/classes/suscripcion.js
@@ -1,43 +1,44 @@
 const webpush = require("web-push");
-const mongoose = require("mongoose");
 const Usuario = require("../models/usuario");
 const vapidKeys = require("../assets/vapid_keys");
 
-webpush.setVapidDetails(
-  "mailtoexample@ejemplo.org",
-  vapidKeys.vapid_public_key,
-  vapidKeys.vapid_private_key
-  );
-  modules.exports = {
+// Función para enviar notif a un usuario con la id correspondiente.
+// #resolve permitir recibir un vector de ids
+function notificar(idusuario, titulo, cuerpo) {
+  Usuario.findOne({ _id: idusuario }).then(usuario => {
+    const allSubscriptions = usuario.suscripciones;
+    console.dir(allSubscriptions);
 
-  notificar: function(idusuario, titulo, cuerpo) {
-    Usuario.findOne({ _id: idusuario }).then(usuario => {
-      const allSubscriptions = usuario.suscripciones;
-      console.dir(allSubscriptions);
+    const notificationPayload = {
+      notification: {
+        title: titulo,
+        body: cuerpo,
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        },
+        actions: [
+          {
+            action: "explore",
+            title: "Go to the site"
+          }
+        ]
+      }
+    };
 
-      const notificationPayload = {
-        notification: {
-          title: titulo,
-          body: cuerpo,
-          vibrate: [100, 50, 100],
-          data: {
-            dateOfArrival: Date.now(),
-            primaryKey: 1
-          },
-          actions: [
-            {
-              action: "explore",
-              title: "Go to the site"
-            }
-          ]
-        }
-      };
+    webpush.setVapidDetails(
+      "mailtoexample@ejemplo.org",
+      vapidKeys.vapid_public_key,
+      vapidKeys.vapid_private_key
+    );
 
-      Promise.all(
-        allSubscriptions.map(sub =>
-          webpush.sendNotification(sub, JSON.stringify(notificationPayload))
-        )
-      );
-    });
-  }
+    Promise.all(
+      allSubscriptions.map(sub =>
+        webpush.sendNotification(sub, JSON.stringify(notificationPayload))
+      )
+    );
+  });
 };
+
+module.exports.notificar = notificar;
