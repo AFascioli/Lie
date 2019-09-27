a31901e170ecbb4cd1235ba4d194afb29ac22729
AFascioli <agufascioli@gmail.com>
2019-09-27 19:39:46 -0300

    bcryptjs changes

--- proyectoicr/backend/routes/usuario.js
+++ proyectoicr/backend/routes/usuario.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Usuario = require("../models/usuario");
 const router = express.Router();
