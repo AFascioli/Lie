2d4921ba4fb0bb9cb79e8fba93b301043aebcff6
AFascioli <agufascioli@gmail.com>
2019-09-27 20:11:03 -0300

    Cambios urls producción.

--- proyectoicr/src/environments/environment.prod.ts
+++ proyectoicr/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'ec2-52-67-105-139.sa-east-1.compute.amazonaws.com'
+  apiUrl: 'http://ec2-52-67-105-139.sa-east-1.compute.amazonaws.com'
   // apiUrl: 'http://localhost:3000'
 };
