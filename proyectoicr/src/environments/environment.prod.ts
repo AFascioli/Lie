937a51b9ab53ece54042d716ea90d338c16e2547
DiegoPonce1995 <poncedp1995@gmail.com>
2019-12-10 18:27:46 -0300

    Cambios SSL

--- proyectoicr/src/environments/environment.prod.ts
+++ proyectoicr/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://ec2-52-67-105-139.sa-east-1.compute.amazonaws.com'
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'ec2-18-228-6-195.sa-east-1.compute.amazonaws.com'
 };
