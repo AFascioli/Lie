a816def1dd4bd78834bf1402d465a3733f0a519c
DiegoPonce1995 <poncedp1995@gmail.com>
2020-01-28 20:04:33 -0300

    Componente creado y esqueleto de grid realizado

--- proyectoicr/src/app/app-routing.module.ts
+++ proyectoicr/src/app/app-routing.module.ts
@@ -31,6 +31,7 @@ import { DatosEstudianteComponent } from "./perfil-estudiante/datos-estudiante/d
 import { TutoresEstudianteComponent } from "./perfil-estudiante/tutores-estudiante/tutores-estudiante.component";
 import { ModificarEventoComponent } from "./eventos/modificar-evento/modificar-evento.component";
 import { VisualizarEventoComponent } from './eventos/visualizar-evento/visualizar-evento.component';
+import { VisualizarAgendaComponent } from './visualizar-agenda/visualizar-agenda.component';
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
@@ -223,6 +224,10 @@ const routes: Routes = [
       {
         path: "preferencias",
         component: PreferenciasComponent
+      },
+      {
+        path: "visualizarAgenda",
+        component: VisualizarAgendaComponent
       }
     ]
   }
---
+++ proyectoicr/src/app/visualizar-agenda/visualizar-agenda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visualizar-agenda',
+  templateUrl: './visualizar-agenda.component.html',
+  styleUrls: ['./visualizar-agenda.component.css']
+})
+export class VisualizarAgendaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
