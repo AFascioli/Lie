49e498f3979a1b9834ba9d20c13f00b4066b04b1
Milena <mile.vilardo714@gmail.com>
2020-02-16 21:39:43 -0300

    avances en css de agenda curso

--- proyectoicr/src/app/visualizar-agenda/agenda.service.ts
+++ proyectoicr/src/app/visualizar-agenda/agenda.service.ts
@@ -4,31 +4,65 @@ import { Injectable } from "@angular/core";
   providedIn: "root"
 })
 export class AgendaService {
-
   //#resolve Metodo para probar, borrar una vez que este hecho todo
-  obtenerMaterias(){
+  obtenerMaterias() {
     var materiaObj = {
-      nombre: "Matemáticas",
+      nombre: "Matemática",
       dia: "Miercoles",
-      inicio: "11:30",
-      fin: "12:15"
+      inicio: "10:30",
+      fin: "11:15"
     };
 
     var materiaObj2 = {
       nombre: "Lengua",
       dia: "Miercoles",
-      inicio: "09:15",
-      fin: "11:30"
+      inicio: "08:40",
+      fin: "10:10"
     };
 
     var materiaObj3 = {
-      nombre: "Fisica",
+      nombre: "Física",
       dia: "Lunes",
       inicio: "07:00",
       fin: "08:30"
     };
 
-    return [materiaObj, materiaObj2, materiaObj3];
+    var materiaObj4 = {
+      nombre: "Historia",
+      dia: "Lunes",
+      inicio: "08:40",
+      fin: "10:10"
+    };
 
+    var materiaObj5 = {
+      nombre: "Biología",
+      dia: "Jueves",
+      inicio: "07:00",
+      fin: "08:30"
+    };
+
+    var materiaObj7 = {
+      nombre: "Biología",
+      dia: "Jueves",
+      inicio: "08:40",
+      fin: "09:25"
+    };
+
+    var materiaObj6 = {
+      nombre: "Matemática",
+      dia: "Viernes",
+      inicio: "07:00",
+      fin: "08:30"
+    };
+
+    return [
+      materiaObj,
+      materiaObj2,
+      materiaObj3,
+      materiaObj4,
+      materiaObj5,
+      materiaObj6,
+      materiaObj7
+    ];
   }
 }
