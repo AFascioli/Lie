2efa4bc59e65c2758870caf840d88a863a5ebc0b
Mmanzi <mayramanzi95@gmail.com>
2020-03-29 15:49:09 -0300

    boton volver en la pantalla de visualizar agenda

--- proyectoicr/backend/app.js
+++ proyectoicr/backend/app.js
@@ -32,30 +32,30 @@ app.use(express.static("../images"));
    lo que corresponda*/
 
 //Conexión a base de producción
-  //mongoose
-  // .connect(
-     //  "mongodb+srv://ComandanteJr:SNcjNuPBMG42lOh1@cluster0-qvosw.mongodb.net/icrdev?retryWrites=true",
-     //  { useNewUrlParser: true, useUnifiedTopology: true }
-   //  )
-    // .then(() => {
-    //   console.log("Conexión a base de datos de producción exitosa");
-  //  })
-    // .catch(() => {
-     //  console.log("Fallo conexión a la base de datos de producción");
-  //  });
+  mongoose
+   .connect(
+       "mongodb+srv://ComandanteJr:SNcjNuPBMG42lOh1@cluster0-qvosw.mongodb.net/icrdev?retryWrites=true",
+       { useNewUrlParser: true, useUnifiedTopology: true }
+     )
+     .then(() => {
+       console.log("Conexión a base de datos de producción exitosa");
+    })
+     .catch(() => {
+       console.log("Fallo conexión a la base de datos de producción");
+    });
 
 // //Conexión a base local
-mongoose
-  .connect("mongodb://127.0.0.1:27017/icr-local", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Conexión a base de datos local exitosa");
-  })
-  .catch(() => {
-    console.log("Fallo conexión a la base de datos local");
-  });
+//mongoose
+  //.connect("mongodb://127.0.0.1:27017/icr-local", {
+    //useNewUrlParser: true,
+    //useUnifiedTopology: true
+ // })
+  //.then(() => {
+    //console.log("Conexión a base de datos local exitosa");
+  //})
+  //.catch(() => {
+    //console.log("Fallo conexión a la base de datos local");
+ // });
 
 
 //Para sacar el deprecation warning de la consola
