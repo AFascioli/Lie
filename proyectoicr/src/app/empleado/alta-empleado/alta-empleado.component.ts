4685686b8a2d79866e0c9866c5f0948196b9b754
Mmanzi <mayramanzi95@gmail.com>
2020-04-11 20:26:42 -0300

    ordenar nacionalidad en registrar empleadoss

--- proyectoicr/src/app/empleado/alta-empleado/alta-empleado.component.ts
+++ proyectoicr/src/app/empleado/alta-empleado/alta-empleado.component.ts
@@ -47,6 +47,9 @@ export class AltaEmpleadoComponent implements OnInit, OnDestroy {
       .pipe(takeUntil(this.unsubscribe))
       .subscribe(nacionalidadesActualizadas => {
         this.nacionalidades = nacionalidadesActualizadas;
+        this.nacionalidades.sort((a, b) =>
+          a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+        );
       });
   }
 
