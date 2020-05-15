257fdf2ddfab75fcfab7a09b6b8525f450efd858
DiegoPonce1995 <poncedp1995@gmail.com>
2020-05-15 16:09:35 -0300

    path de key corregida

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
      production : {
       user: 'ubuntu',
       host:'ec2-18-223-190-44.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/authorized_keys',
+      key: '/home/ubuntu/.ssh/authorized_keys',
       ref: 'origin/testpm2',
       repo : 'git@github.com:AFascioli/Lie.git',
       path : '.',
