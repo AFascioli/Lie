bc5377d83224a0793b9f7fb654e5eb44dfc22aa2
DiegoPonce1995 <poncedp1995@gmail.com>
2020-05-15 16:54:39 -0300

    path arreglado version 3

--- proyectoicr/ecosystem.config.js
+++ proyectoicr/ecosystem.config.js
@@ -11,8 +11,8 @@ module.exports = {
       key: 'C:/Users/Administrador/Downloads/UTN/PF/Keys_ec2/softwarelie.pem',
       ref: 'origin/testpm2',
       repo : 'git@github.com:AFascioli/Lie.git',
-      path : '/home/ubuntu/source/proyectoicr',
-      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js'
+      path : '/home/ubuntu',
+      'post-deploy' : 'npm install ./proyectoicr && pm2 startOrRestart ./proyectoicr/ecosystem.config.js'
     }
   }
 };
