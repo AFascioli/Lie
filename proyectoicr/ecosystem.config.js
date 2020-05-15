54bc6bc7c3c28ecff5ddfa198825041f6b4667f2
DiegoPonce1995 <poncedp1995@gmail.com>
2020-05-15 16:43:14 -0300

    path arreglado, cambiado post-deploy

---
+++ proyectoicr/ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps : [{
+    name: 'lie',
+    script: 'server.js',
+    cwd: './proyectoicr/backend'
+  }],
+  deploy :{
+     production : {
+      user: 'ubuntu',
+      host:'ec2-18-223-190-44.us-east-2.compute.amazonaws.com',
+      key: 'C:/Users/Administrador/Downloads/UTN/PF/Keys_ec2/softwarelie.pem',
+      ref: 'origin/testpm2',
+      repo : 'git@github.com:AFascioli/Lie.git',
+      path : '/home/ubuntu/source/proyectoicr',
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+};
