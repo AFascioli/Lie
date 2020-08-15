7ee970dd62b295fcd8d62b519dc82ba798703af1
FlorenciaBargiano <bargianoflorencia@outlook.com>
2020-08-15 10:49:50 -0300

    agregados estados a modelos

--- proyectoicr/backend/models/cicloLectivo.js
+++ proyectoicr/backend/models/cicloLectivo.js
@@ -1,17 +1,11 @@
 const mongoose = require("mongoose");
 
 const cicloLectivoSchema = mongoose.Schema({
-  fechaInicioInscricpcion: Date,
-  fechaFinInscripcion: Date,
-  fechaInicioPrimerTrimestre: Date,
-  fechaFinPrimerTrimestre: Date,
-  fechaInicioSegundoTrimestre: Date,
-  fechaFinSegundoTrimestre: Date,
-  fechaInicioTercerTrimestre: Date,
-  fechaFinTercerTrimestre: Date,
-  fechaInicioExamenes: Date,
-  fechaFinExamenes: Date,
-  año: Number
+  horarioLLegadaTarde: Number,
+  horarioRetiroAnticipado: Number,
+  cantidadFaltasSuspension: Number,
+  cantidadMateriasInscripcionLibre: Number,
+  año: Number,
 });
 
 module.exports = mongoose.model(
